d3d97a67d9f0974f00a66903f3c2d4611d2fc323
Kareem Nasser <151379758+KareemNasser08@users.noreply.github.com>
2024-09-11 09:49:01 +0300

    test

---
+++ src/app/Features/auth/Services/auth.service-KarimNasser.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IsignIn } from '../interfaces/auth';
+import { jwtDecode } from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private _HttpClient: HttpClient) {
+    // this.onGetUserData();
+  }
+
+  // declerations
+  role: string | any = '';
+
+
+  onSignIn(sigInForm: IsignIn): Observable<any> {
+    return this._HttpClient.post(`admin/users/login`, sigInForm);
+  }
+
+  onGetUserData() {
+    let decodedToken: any = localStorage.getItem('eToken');
+    let encodedToken: any = jwtDecode(decodedToken);
+
+    localStorage.setItem("userId", encodedToken._id);
+    localStorage.setItem('userName', encodedToken.userName);
+    localStorage.setItem("userRole", encodedToken.role);
+    this.onGetUserRole();
+  }
+
+  onGetUserRole() {
+    if (
+      localStorage.getItem('eToken') !== null
+      &&
+      localStorage.getItem("userRole") !== null
+    ) {
+      this.role = localStorage.getItem("userRole");
+    }
+  }
+
+}
