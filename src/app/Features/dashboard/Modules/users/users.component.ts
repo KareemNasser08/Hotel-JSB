a46daf495c6a80e160f51a6ad0dc5a344c8bdb06
M-Naill <mahmoud.naill7@gmail.com>
2024-09-14 03:36:03 +0300

    update users

--- src/app/Features/dashboard/Modules/users/users.component.ts
+++ src/app/Features/dashboard/Modules/users/users.component.ts
@@ -1,19 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  tableData: any;
 
-  constructor(private _Router: Router,){
-    
-  }
+  headingList = [
+    {headerTitle: 'User Name', headerKey:'userName'},
+    {headerTitle: 'Image', headerKey:'profileImage'},
+    {headerTitle: 'email', headerKey:'email'},
+    {headerTitle: 'Country', headerKey:'country'},
+    {headerTitle: 'Phone Number', headerKey:'phoneNumber'},
+    {headerTitle: 'Role', headerKey:'role'},
+  ];
 
-  gorooms(){
-    this._Router.navigate(['/dashboard/Rooms/add-edit-room']);
-  }
+  constructor(
+    private _UsersService: UsersService,
+
+  ) { }
   
+  ngOnInit(): void {
+    this.onGetAllUsers();
+  }
+
+  onGetAllUsers() {
+    let params = {
+      page: 1,
+      size: 10000
+    };
+    this._UsersService.getAllUsers(params).subscribe({
+      next: (res) => {
+        this.tableData = res.data.users.map((user: { facilities: any[]; }) => {
+          return {
+            ...user,
+          };
+        });
+      }
+    });
+  }
+
+
 }
