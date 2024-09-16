1e9468f4fa2942cebd98d9ca9f98106cd2a82ccf
M-Naill <mahmoud.naill7@gmail.com>
2024-09-16 18:23:13 +0300

    add sections

---
+++ src/app/Features/landing/components/ads-1/ads-1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ads-1',
+  templateUrl: './ads-1.component.html',
+  styleUrls: ['./ads-1.component.scss']
+})
+export class Ads1Component {
+
+}
